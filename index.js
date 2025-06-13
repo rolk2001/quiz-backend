52668de1693e4bd1d29d732ee18735f6cb6a24db
rolk2001 <tadmbayero@gmail.com>
2025-06-13 06:36:18 +0100

    Ajout de dotenv + configuration MONGO_URI depuis .env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -8,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connexion à MongoDB
-mongoose.connect('mongodb://localhost:27017/quiz-app', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
